98fa1c51bf360c21b85610d23a8c8a5116b392ec
prateekbashista <bashistaprateek@gmail.com>
2024-07-01 23:20:48 -0400

    completed implementation of ROB

--- ROB.cpp
+++ ROB.cpp
@@ -1,24 +1,145 @@
 #include <includes.h>
 
+
+// The rob will be implemented using a queue.
+// The queue will be custom, and thus, it will be made up of rob_entry
 int stall_rob;
 
 struct rob_entry{
-    BYTE opcode;
+    uint64_t id;
     REG PC;
+    BYTE opcode;
     REG logical_reg;
     REG previous_reg;
+    WORD VALUE;
     BYTE exception : 1;
+    struct rob_entry *next;
 };
 
 class ROB{
 
     rob_entry *head, *tail;
-    static int capacity;
-    static int current;
+    int capacity;
+    int current;
+    int new_id;
+
     public:
-    void add_to_rob();
-    void stride();
-    void reset_rob();
-    
+    ROB() : head(nullptr), tail(nullptr)
+    {
+        capacity = 1;
+        current = 0;
+        new_id = 0;
+        stall_rob = 0;
+    }
+    ROB(int capacity) : head(nullptr), tail(nullptr)
+    {
+        this->capacity = capacity;
+        current = 0;
+        new_id = 0;
+        stall_rob = 0;
+    }
+    void add_to_rob(REG PC, BYTE opcode, REG logical_reg, REG previous_reg, REG VALUE)
+    {
+        struct rob_entry *new_entry = new struct rob_entry;
+        new_id++;
+        new_entry->id = new_id;
+        new_entry->PC = PC;
+        new_entry->opcode = opcode;
+        new_entry->logical_reg = logical_reg;
+        new_entry->previous_reg = previous_reg;
+        new_entry->VALUE = VALUE;
+
+        if(current = 0)
+        {
+            head = tail = new_entry;
+            stall_rob = 0;
+            current++;
+        }  
+        else if(current == capacity)
+        {
+            stall_rob = 1;
+            delete new_entry;
+        }
+        else
+        {   
+            stall_rob = 0;
+            tail->next  = new_entry;
+            tail = new_entry;
+            capacity++;
+        }
+    }
+    void reset_rob()
+    {
+        ROB(capacity);
+    }
+    void exception_set(REG PC)
+    {
+        struct rob_entry* iter = head;
+
+        while(iter!=nullptr)
+        {
+            if(iter->PC == PC)
+            {
+                iter->exception = 1;
+                break;
+            }
+            iter = iter->next;
+        }    
+
+    }
+    void retire_value(REG PC, WORD VALUE)
+    {
+        struct rob_entry *iter = head;
+
+        while(iter!=nullptr)
+        {
+            if(iter->PC == PC)
+            {
+                iter->VALUE = VALUE;
+                break;
+            }
+            iter = iter->next;
+        }
+    }
+    struct rob_entry* commit_from_rob()
+    {
+        if(exception_handle() == true)
+        {
+                
+            struct rob_entry *del = nullptr;
+            if(current == 0)
+            {
+                std::cout<<"\n C++ Error: Trying to decrement an empty ROB";
+            }
+            else if(head->next == nullptr)
+            {
+                del  = head;
+                delete head;
+                head = tail = nullptr;
+                current--;
+            }
+            else
+            {
+                struct rob_entry *freer = head;
+                head = head->next;
+                delete freer;
+                current--;
+            }
+
+            return del;
+        }
+        else
+        {
+            return nullptr; 
+        }
+    }
+    bool exception_handle()
+    {
+        if(head->exception == 1)
+        {
+            head = tail;
+        }
+        return true;
+    }
     
 };
